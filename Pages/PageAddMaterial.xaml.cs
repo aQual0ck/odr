6fccf293e6383134196e62222943f91c78c30882
aQual0ck <glassplayer1@gmail.com>
2024-11-25 12:55:41 +0300

    add material in progress

--- Pages/PageAddMaterial.xaml.cs
+++ Pages/PageAddMaterial.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace odr.Pages
 {
@@ -20,9 +22,60 @@ namespace odr.Pages
     /// </summary>
     public partial class PageAddMaterial : Page
     {
+        private string _filepath;
+        private Classes.Material mat;
+        private List<Classes.Supplier> _sup = new List<Classes.Supplier>();
         public PageAddMaterial()
         {
             InitializeComponent();
+            cmbSupplier.SelectedValuePath = "Title";
+            cmbSupplier.DisplayMemberPath = "Title";
+            cmbSupplier.ItemsSource = Classes.DBModel.entObj.Supplier.ToList();
+        }
+
+        private void cmbSupplier_TextChanged(object sender, RoutedEventArgs e)
+        {
+            cmbSupplier.IsDropDownOpen = true;
+            cmbSupplier.ItemsSource = Classes.DBModel.entObj.Supplier.Where(s => s.Title.ToLower().Contains(cmbSupplier.Text.ToLower())).ToList();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            mat = new Classes.Material()
+            {
+                Title = txbTitle.Text,
+                CountInPack = Convert.ToInt32(txbCountInPack.Text),
+                Unit = txbUnit.Text,
+                CountInStock = Convert.ToInt32(txbCountInStock.Text),
+                MinCount = Convert.ToInt32(txbMinCount.Text),
+                Cost = Convert.ToInt32(txbCost.Text),
+
+                Supplier = _sup
+            };
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            _sup.Add(cmbSupplier.SelectedItem as Classes.Supplier);
+            if (_sup.Count == 1)
+                tbSupplier.Text += $"{cmbSupplier.SelectedValue}";
+            else
+                tbSupplier.Text += $", {cmbSupplier.SelectedValue}";
+            cmbSupplier.ItemsSource = Classes.DBModel.entObj.Supplier.ToList();
+        }
+
+        private void btnPick_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "D:\\Projects\\Csharp\\odr\\materials";
+
+            bool? result = ofd.ShowDialog();
+
+            if (result == true)
+            {
+                _filepath = ofd.FileName.Replace(ofd.FileName.Substring(0, ofd.FileName.IndexOf("\\materials")), "");
+                txbImage.Text = _filepath;
+            }
         }
     }
 }
